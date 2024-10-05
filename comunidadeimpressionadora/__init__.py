7bf697a76fff9139ffb31d14fcf40ac04879e582
angemydelson <delsonysuperdel@gmail.com>
2024-10-04 21:36:30 -0300

    :sparkles: feat: Adicionando a estrutura básica do projeto

---
+++ comunidadeimpressionadora/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for, request, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
+
+app.config['SECRET_KEY'] = '9a2b9c1232582c7952c3dd35b018441b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+database = SQLAlchemy(app)
+
+from comunidadeimpressionadora import routes
