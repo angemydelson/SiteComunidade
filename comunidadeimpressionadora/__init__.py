3a390299dd9d38dceb650612f13857cca4701059
angemydelson <delsonysuperdel@gmail.com>
2024-10-07 00:24:03 -0300

    :sparkles: feat: adiciondando coisas importantes

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -9,5 +11,11 @@ app.config['SECRET_KEY'] = '9a2b9c1232582c7952c3dd35b018441b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'alert-info'
 
 from comunidadeimpressionadora import routes
+from comunidadeimpressionadora.models import Usuario
+
--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, redirect, url_for, flash, request
-from comunidadeimpressionadora import app
-from comunidadeimpressionadora.forms import FormLogin, FormCriarConta
+from comunidadeimpressionadora import app, database, bcrypt
+from comunidadeimpressionadora.forms import FormLogin, FormCriarConta, FormEditarPerfil
 from comunidadeimpressionadora.models import Usuario
-lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
+from flask_login import login_user, logout_user, current_user, login_required
 
 @app.route('/')
 def home():
@@ -13,7 +13,9 @@ def contato():
     return render_template('contato.html')
 
 @app.route('/usuarios')
+@login_required
 def usuarios():
+    lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -22,11 +24,67 @@ def login():
     form_criarconta = FormCriarConta()
 
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
-        return redirect(url_for('home'))
-    
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=form_login.lembrar_dados.data)
+            par_next = request.args.get('next')
+            if par_next:
+                return redirect(par_next)
+            else:
+                return redirect(url_for('home'))
+
+        else:
+            flash(f'Falha no Login! E-mail ou Senha Incorretos', 'alert-danger')
+
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        usuario = Usuario(username=form_criarconta.username.data, 
+                        email=form_criarconta.email.data, 
+                        senha=senha_cript)
+        database.session.add(usuario)
+        database.session.commit()
         flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
         return redirect(url_for('home'))
 
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
+
+@app.route('/sair')
+@login_required
+def sair():
+    logout_user()
+    flash(f'Logout Feito com Sucesso!', 'alert-success')
+    return redirect(url_for('home'))
+
+@app.route('/perfil')
+@login_required
+def perfil():
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    return render_template('perfil.html', foto_perfil=foto_perfil)
+
+@app.route('/post/criar')
+@login_required
+def criar_post():
+    return render_template('criarpost.html')
+
+@app.route('/perfil/editar', methods=['GET', 'POST'])
+@login_required
+def editar_perfil():
+    form = FormEditarPerfil()
+
+    # Pré-popular o formulário com os dados do usuário atual
+    if request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    
+    # Se o formulário for submetido e for válido
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        # Salve as alterações no banco de dados
+        database.session.commit()
+        flash('Perfil atualizado com sucesso!', 'alert-success')
+        return redirect(url_for('perfil'))
+
+    return render_template('editarperfil.html', foto_perfil=foto_perfil, form=form)
